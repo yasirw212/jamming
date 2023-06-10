20dfc94c0ed670fd7a4e0dd2536aa4b9c1ecaabb
yasirw212 <williamsyasir90@gmail.com>
2023-06-10 16:59:06 -0500

    Created site navabr.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './styles.css'
+import logo from '../../assets/icons8-spotify-50.png'
 
 export default function Navbar() {
   return (
     <div className='navbar' >
-        <h1>Gangnem</h1>
+        <h1 className='navbar-header'>Jamming </h1>
+        <a href="#" className="navbar-brand"><img height={40} width={40} src={logo} alt="Spotify logo" /></a>
     </div>
   )
 }
