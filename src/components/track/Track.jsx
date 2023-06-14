6556cc00d882107fb5884cb4ddece721016649ba
yasirw212 <williamsyasir90@gmail.com>
2023-06-14 13:43:06 -0500

    Created a component to display songs added to playlist and allow users to then add that playlist to their spotify account

--- src/components/track/Track.jsx
+++ src/components/track/Track.jsx
@@ -3,24 +3,25 @@ import { SpotifyContext } from '../../context/context'
 import { Add } from '@mui/icons-material'
 import { HorizontalRule } from '@mui/icons-material'
 import { IconButton } from '@mui/material'
+import {nanoid} from "nanoid"
 
 const Track = ({track, name, artist, album, inPlayList}) => {
   const {addToPlaylist, removeFromPlaylist} = React.useContext(SpotifyContext)
 
   return (
-    <div className='track-card-component'>
+    <div className='track-card-component' key={nanoid()}>
         <div className="text">
-          <h3 className='title'>{name}</h3>
-          <p className='subtext'>{`${artist} | ${album}`}</p>
+          <h3 className='track-title'>{name}</h3>
+          <p className='track-subtext'>{`${artist} | ${album}`}</p>
         </div>
         {
           inPlayList ?
-          <IconButton onClick={() => removeFromPlaylist(track)} >
-            <HorizontalRule />
+          <IconButton  onClick={() => removeFromPlaylist(track)} >
+            <HorizontalRule sx={{color: '#d7d7d7', '&:hover':{background: 'rgba(255,255,255, 0.3)', borderRadius: '50%', padding: '.15em .075em'}}}  />
           </IconButton>
           :
           <IconButton onClick={() => addToPlaylist(track)} >
-            <Add />
+            <Add sx={{color: '#d7d7d7', '&:hover':{background: 'rgba(255,255,255, 0.3)', borderRadius: '50%', padding: '.15em .075em'}}} />
           </IconButton>
         }
     </div>
